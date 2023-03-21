83077d5f4fab08e7961954fd6e51fd1d3daebed0
ketki.chaudhary <ketki.chaudhary@kxrdev.com>
2023-03-21 09:15:08 +0800

    Updated to follow default name convention

--- create_table_transformed.sql
+++ create_table_transformed.sql
@@ -8,56 +8,56 @@ CREATE TABLE employees (
 	salary numeric
 );
 
-DROP TRIGGER IF EXISTS verify_birth_date on employees;
-DROP FUNCTION IF EXISTS birth_date;
+DROP TRIGGER IF EXISTS employees_birth_date_check on employees;
+DROP FUNCTION IF EXISTS verify_employees_birth_date;
 
-CREATE FUNCTION birth_date()
+CREATE FUNCTION verify_employees_birth_date()
 RETURNS trigger AS $$
 BEGIN
 IF new.birth_date > '1900-01-01' THEN
     RETURN NEW;
 ELSE 
-    RAISE EXCEPTION 'Invalid birth_date';
+    RAISE EXCEPTION 'Invalid birth_date in employees';
 END IF;
 END;
 $$
 LANGUAGE 'plpgsql';
 
-CREATE TRIGGER verify_birth_date BEFORE INSERT OR UPDATE ON employees
-            FOR EACH ROW EXECUTE PROCEDURE birth_date();
+CREATE TRIGGER employees_birth_date_check BEFORE INSERT OR UPDATE ON employees
+            FOR EACH ROW EXECUTE PROCEDURE verify_employees_birth_date();
 
-DROP TRIGGER IF EXISTS verify_joined_date on employees;
-DROP FUNCTION IF EXISTS joined_date;
+DROP TRIGGER IF EXISTS employees_joined_date_check on employees;
+DROP FUNCTION IF EXISTS verify_employees_joined_date;
 
-CREATE FUNCTION joined_date()
+CREATE FUNCTION verify_employees_joined_date()
 RETURNS trigger AS $$
 BEGIN
 IF new.joined_date > new.birth_date THEN
     RETURN NEW;
 ELSE 
-    RAISE EXCEPTION 'Invalid joined_date';
+    RAISE EXCEPTION 'Invalid joined_date in employees';
 END IF;
 END;
 $$
 LANGUAGE 'plpgsql';
 
-CREATE TRIGGER verify_joined_date BEFORE INSERT OR UPDATE ON employees
-            FOR EACH ROW EXECUTE PROCEDURE joined_date();
+CREATE TRIGGER employees_joined_date_check BEFORE INSERT OR UPDATE ON employees
+            FOR EACH ROW EXECUTE PROCEDURE verify_employees_joined_date();
 
-DROP TRIGGER IF EXISTS verify_salary on employees;
-DROP FUNCTION IF EXISTS salary;
+DROP TRIGGER IF EXISTS employees_salary_check on employees;
+DROP FUNCTION IF EXISTS verify_employees_salary;
 
-CREATE FUNCTION salary()
+CREATE FUNCTION verify_employees_salary()
 RETURNS trigger AS $$
 BEGIN
 IF new.salary > 0 THEN
     RETURN NEW;
 ELSE 
-    RAISE EXCEPTION 'Invalid salary';
+    RAISE EXCEPTION 'Invalid salary in employees';
 END IF;
 END;
 $$
 LANGUAGE 'plpgsql';
 
-CREATE TRIGGER verify_salary BEFORE INSERT OR UPDATE ON employees
-            FOR EACH ROW EXECUTE PROCEDURE salary();
+CREATE TRIGGER employees_salary_check BEFORE INSERT OR UPDATE ON employees
+            FOR EACH ROW EXECUTE PROCEDURE verify_employees_salary();
